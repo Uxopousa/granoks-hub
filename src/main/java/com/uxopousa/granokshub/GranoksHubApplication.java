e4d595941f23ad35565326b391b5aba620a484ec
Uxopousa <uxopousa@gmail.com>
2025-06-11 21:51:33 +0200

    Importar esqueleto Spring Initializr

---
+++ src/main/java/com/uxopousa/granokshub/GranoksHubApplication.java
@@ -0,0 +1,13 @@
+package com.uxopousa.granokshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranoksHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranoksHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uxopousa/granokshub/GranoksHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uxopousa.granokshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranoksHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
